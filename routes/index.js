2410875ce5aa125068c9fdd26a743d1011cb8fb3
jherreros <juanherreros4@gmail.com>
2015-04-28 13:44:13 +0200

    Créditos

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bienvenido a Quiz' });
 });
 
+/* GET author page. */
+router.get('/author', function(req, res, next) {
+  res.render('author', { title: 'Página de créditos' });
+});
+
 module.exports = router;
