db8f0247c33b3e957de55b34fede25f53e450f81
jherreros <juanherreros4@gmail.com>
2015-05-10 18:41:52 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,9 @@ router.get('/author', function(req, res, next) {
 router.get('/quizes/question', quizController.question);
 // router.get('/quizes/answer', quizController.answer);
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+
 // Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
