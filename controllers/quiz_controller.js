29d1d8730cf37dc94781adffb74de652cd6b3e88
jherreros <juanherreros4@gmail.com>
2015-05-14 11:17:49 +0200

    Creación de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -57,13 +57,20 @@ exports.answer= function(req, res) {
 		res.render('quizes/answer',	{quiz: req.quiz, respuesta: resultado});
 };
 
+// GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build( //crea objeto quiz
+		{pregunta: "Pregunta", respuesta: "respuesta"}
+	);
+	res.render('quizes/new', {quiz: quiz});
+};
 
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
 
-// // GET /quizes?search
-// exports.search= function(req,res) {
-// 	models.Quiz.findAll({where: ["pregunta like ?", search]}).then(
-// 		function(quizes){
-// 			res.render('quizes/index', {quizes: quizes});
-// 		}
-// 	).catch(function(error) {next(error);})
-// };
+// guarda en DB los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	}) // Redirección HTTP (URL relativo) lista de preguntas
+};
