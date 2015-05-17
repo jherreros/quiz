d7211cbe49712b624754685762558988d12fcf14
jherreros <juanherreros4@gmail.com>
2015-05-17 13:04:25 +0200

    Autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,6 +21,11 @@ router.get('/quizes/question', quizController.question);
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload :quizId
 
+// Definicion de rutas de session
+router.get('/login', sessionController.new); //formulario login
+router.post('/login', sessionController.create); // crear sesion
+router.get('/logout', sessionController.destroy); // destruir sesion
+
 // Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
@@ -30,6 +36,7 @@ router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d+)', quizController.update);
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 
